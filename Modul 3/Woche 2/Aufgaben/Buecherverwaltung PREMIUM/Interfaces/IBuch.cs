4fa1ce6ff50dcc9b6a91ac2ca8e3bd7bbb8bbc40
RomanPeterP <roman@aspnetprofi.com>
2025-01-16 15:41:07 +0100

    del

--- Modul 3/Woche 2/Aufgaben/Buecherverwaltung PREMIUM/Interfaces/IBuch.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Buecherverwaltung.Interfaces
-{
-    public interface IBuch
-    {
-        public int Id { get; set; }
-        public string Titel { get; set; }
-        public string Autor { get; set; }
-    }
-}
