1343b0df0bc76ebde207d8dd6d70f37b2e4dc4a7
RomanPeterP <roman@aspnetprofi.com>
2025-02-03 12:48:17 +0100

    upd

--- Modul 2/Woche 1-4/MyShopApp/Models/Order.cs
+++ Modul 2/Woche 1-4/MyShopApp/Models/Order.cs
@@ -12,9 +12,21 @@ namespace MyShop.Models
         {
             if (OrderItems == null)
                 OrderItems = new List<OrderItem>();
-            if (OrderItems.Any(bp => bp.Product.ProductId == orderItem.Product.ProductId))
+
+            if (Any())
                 throw new Exception(orderItem.Product.Name + " existiert bereits in dieser Bestellung!");
+
+            //if (OrderItems.Any(bp => bp.Product.ProductId == orderItem.Product.ProductId))
+            //    throw new Exception(orderItem.Product.Name + " existiert bereits in dieser Bestellung!");
             OrderItems.Add(orderItem);
+
+            bool Any()
+            {
+                for (var i = 0; i < OrderItems.Count; i++)
+                    if (OrderItems[i].Product.ProductId == orderItem.Product.ProductId)
+                            return true;
+                return false;
+            }
         }
     }
 }
