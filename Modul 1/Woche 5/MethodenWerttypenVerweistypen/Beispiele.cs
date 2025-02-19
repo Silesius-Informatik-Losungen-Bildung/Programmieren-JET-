4b992aeb33050585e76cefea57c94233623cc6cf
RomanPeterP <roman@aspnetprofi.com>
2025-02-19 14:48:36 +0100

    upd

---
+++ Modul 1/Woche 5/MethodenWerttypenVerweistypen/Beispiele.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+namespace MethodenWerttypenVerweistypen
+{
+    public static class Beispiele
+    {
+        static decimal GetNeuPreis()
+        {
+            return 123.90m;
+        }
+
+        static decimal GetNeuPreis2(int zahl)
+        {
+            throw new ArgumentException();
+        }
+
+        static void GenerierePasswort()
+        {
+            var passwort = "jkkf8343jhkf#hjkhdsf";
+        }
+
+        static decimal GetNeuPreis(decimal altpreis)
+        {
+            return (altpreis + 1.0m);
+        }
+
+        static void Verdopple(ref int zahl)
+        {
+            zahl *= 2;
+        }
+
+        public static void Verdopple(int zahl, out int ergebnis)
+        {
+            ergebnis = zahl *= 2;
+        }
+
+        static int Addiere(int zahl1, int zahl2)
+        {
+            return zahl1 + zahl2;
+        }
+
+        static double Addiere(double zahl1, double zahl2)
+        {
+            return zahl1 + zahl2;
+        }
+
+        static string Addiere(string wort1, string wort2)
+        {
+            return wort1 + wort2;
+        }
+
+        public static string Addiere(double zahl, string wort)
+        {
+            return zahl + wort;
+        }
+
+        public static string GetName(string vorname = "Franz", string nachname = "Huber")
+        {
+            return vorname + " " + nachname;
+        }
+
+        static string GetPerson(string vorname = "Franz", string nachname = "Huber", string stadt = "Wien")
+        {
+            return vorname + "  " + nachname + "aus " + stadt;
+        }
+
+        public static string MacheKommaseparierteZeichenkette(params string[] woerter)
+        {
+            return string.Join(',', woerter);
+        }
+        static string Umkehren(string eingabe)
+        {
+            
+            
+            char[] ZeichenUmkehren(string str)
+            {
+                char[] chars = str.ToCharArray();
+                Array.Reverse(chars);
+                return chars;
+            }
+
+            return new string(ZeichenUmkehren(eingabe));
+        }
+
+
+            public static int Fakultät(int n)
+        {
+            if (n < 0) throw new ArgumentException("n muss positiv sein");
+            if (n == 0) return 1; // Abbruchbedingung
+
+            return n * Fakultät(n - 1); // Rekursiver Aufruf
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Modul 1/Woche 5/MethodenWerttypenVerweistypen/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace MethodenWerttypenVerweistypen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //var person = new Person() { Name = "Müller" };
+            //ChangePersonNameToHuber(person);
+            //Console.WriteLine(person.Name);
+
+            //ChangePersonNameToHuber(person);
+            //Console.WriteLine(person.Name);
+
+
+            //int zahl = 1;
+            //ChangeZahl(ref zahl);
+            //Console.WriteLine(zahl);
+
+            //int zahl = 45;
+
+            //Beispiele.Verdopple(zahl: zahl, out int ergebnis);
+            ////Console.WriteLine(ergebnis);
+
+            //var str = Beispiele.GetName(nachname: "Müller", vorname: "Franz");
+
+            //Beispiele.Addiere(wort: "", zahl: 0);
+
+            //var str3 = Beispiele.MacheKommaseparierteZeichenkette("Fluss", "Meer", "Fische");
+
+
+            Beispiele.Fakultät(10);
+        }
+
+
+        public static void ChangeZahl(ref int z)
+        {
+            z = z + 1;
+        }
+
+
+        public static void ChangePersonNameToHuber2(Person p)
+        {
+            const string name = "Huber";
+            p.Name = name;
+        }
+
+        public static Person ChangePersonNameToHuber(Person person)
+        {
+            const string name = "Huber";
+            person.Name = name;
+            return person;
+        }
+
+
+
+        public class Person()
+        {
+            public string Name { get; set; }
+        }
+    }
+}
