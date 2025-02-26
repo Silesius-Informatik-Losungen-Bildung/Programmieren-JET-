914a949329467293424e53b825c3a78bc579b92b
RomanPeterP <roman@aspnetprofi.com>
2025-02-26 17:22:24 +0100

    upd

---
+++ Modul 1/Woche 6/OOP1/Auto.cs
@@ -0,0 +1,104 @@
+﻿namespace Test
+{
+    public class Auto
+    {
+        private string _markeModell;
+        private int _kmStand;
+        private string _fahrgestellnummer;
+
+        public Auto(string markeModell)
+        {
+            _markeModell = markeModell;
+        }
+
+        public Auto(int dd)
+        {
+            _kmStand = dd;
+        }
+
+
+        // Leerer Konstruktor
+
+        //public Auto()
+        //{
+
+        //}
+
+
+        // Property mit Zugriff auf privates Field
+        public Auto(string markeModell, int kmStand)
+        {
+            _markeModell = markeModell;
+            _kmStand = kmStand;
+            Id = Guid.NewGuid().ToString();
+        }
+
+        // Property mit init(Einmalige Initialisierung)
+        public string Id { get; init; }
+
+        
+        // Kurzschreibweise für Getter
+        public string Fahrgestellnummer => _fahrgestellnummer;
+
+        
+        // Property mit Standard-Wert
+        public byte TuereAnzahl { get; set; } = 5;
+
+        public string MarkeModell
+        {
+            get { return _markeModell; }
+            set { _markeModell = value; }
+        }
+
+        // Auto-Implemented Property
+        public string Farbe { get; set; }
+
+        // Public Variable
+        public int BauJahr;
+
+        // Public Variable
+        public int KmStand;
+       
+        // Nur-Getter (berechneter Getter)
+        public bool IsGebrauchtWagen
+        {
+            get {
+                return (_kmStand > 10);
+            }
+        }
+
+        // Nur-Setter
+        public bool IsMeinAuto
+        {
+            set
+            {
+                IsMeinAuto = value;
+            }
+        }
+
+        // Methode (Verhalten)
+        public void Fahren()
+        {
+            // LOgik, Auto in Begung zu setze
+            Console.WriteLine("Auto fährt");
+        }
+
+        // Methode (Verhalten)
+        public void Huppen()
+        {
+
+        }
+
+        // Methode (Verhalten)
+        public void Bremsen()
+        {
+
+        }
+
+        // Destruktor für den Notfall
+        ~Auto()
+        {
+            
+        }
+    }
+}
---
+++ Modul 1/Woche 6/OOP1/Program.cs
@@ -0,0 +1,50 @@
+﻿using Test;
+
+class Programm
+{
+    static void Main(string[] args)
+    {
+
+        // Auto1: Vor der Einführung von Objektorientierung
+        int kmStandAuto1 = 100;
+        int bauJahrAuto1 = 1999;
+        var markeModell_Auto1 = "Suzuki Swift";
+        var autoFarbe_Auto1 = "blau";
+        var autoBauJahr_Auto1 = 1999;
+        var autoKmStand_Auto1 = 160000;
+        var AutoIsGebrauchtWagen_Auto1 = kmStandAuto1 > 10;
+
+        // Auto2: Vor der Einführung von Objektorientierung
+        int kmStandAuto2 = 100;
+        int bauJahrAuto2 = 1999;
+        var markeModell_Auto2 = "Suzuki Swift";
+        var autoFarbe_Auto2 = "blau";
+        var autoBauJahr_Auto2 = 1999;
+        var autoKmStand_Auto2 = 160000;
+        var AutoIsGebrauchtWagen_Auto2 = kmStandAuto2 > 10;
+
+
+
+        // Auto 1 mit Objektorientierung
+        Auto auto1 = new Auto("Suzuki Swift");
+        //to1.BauJahr = 2012;
+        auto1.MarkeModell = "markeModell_Auto2;";
+
+        Console.WriteLine(auto1.KmStand);
+        auto1.KmStand = 10000;
+        //auto1.IsGebrauchtWagen = true;
+        Console.WriteLine(auto1.IsGebrauchtWagen);
+
+
+        // Auto 2 mit Objektorientierung
+        Auto auto2 = new Auto("Opel XX", 223);
+        auto2.IsMeinAuto = true;
+        Console.WriteLine(auto2.IsMeinAuto);
+
+
+    }
+
+
+
+}
+
