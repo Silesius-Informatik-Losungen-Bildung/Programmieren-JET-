023b8a91ef327a7f3b815671076521d39f784c47
RomanPeterP <roman@aspnetprofi.com>
2025-02-27 16:10:09 +0100

    upd

---
+++ Modul 3/Woche 8/Strukturen/Bruch.cs
@@ -0,0 +1,16 @@
+﻿namespace Strukturen
+{
+    struct Bruch
+    {
+        public int Zähler { get; }
+        public int Nenner { get; }
+
+        public Bruch(int zähler, int nenner)
+        {
+            Zähler = zähler;
+            Nenner = nenner == 0 ? 1 : nenner;
+        }
+
+        public double AlsDezimalwert() => (double)Zähler / Nenner;
+    }
+}
