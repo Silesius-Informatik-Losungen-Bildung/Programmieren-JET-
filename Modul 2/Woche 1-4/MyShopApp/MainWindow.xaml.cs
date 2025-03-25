620f60eac60f1c6ad88f106cab8b41023d6e31e6
RomanPeterP <roman@aspnetprofi.com>
2025-03-25 19:17:26 +0100

    upd

--- Modul 2/Woche 1-4/MyShopApp/MainWindow.xaml.cs
+++ Modul 2/Woche 1-4/MyShopApp/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using MyShop.Models;
 using MyShopApp.Logic;
+using System.IO;
+using System.Text.Json;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -13,60 +15,22 @@ namespace MyShopApp
             // Elemente der grafischen Oberfläche werden instaziert
             InitializeComponent();
 
-            
+
             // Company instanzieren
-            var company = new Company
-            {
-                Name = "Billa",
-                Email = "bill@billa.xy",
-                Phone = "3432432432",
-                PostCode = 1140,
-                City = "Wien",
-                Street = "Kendlerstr 34",
-                Uid = "AT123456789",
-                Website = "www.example.com",
-                Contact = "Alexander",
-            };
+            var company = JsonSerializer.Deserialize<Company>(File.ReadAllText("Data\\company.json"));
 
             // Lieferanten für Produkte instanzieren
-            var supplier1 = new Supplier
-            {
-                Name = "Lieferant 1",
-                Email = "aaa@bbb.at",
-                Street = " Kendlerstr 34",
-                City = "Wien",
-                PostCode = 1140,
-                Phone = "32423423"
-            };
-            var supplier2 = new Supplier
-            {
-                Name = "Lieferant 2",
-                Email = "dsds@dsdsd.at",
-                Street = " Kendlerstr 34",
-                City = "Wien",
-                PostCode = 1140,
-                Phone = "65487789789",
-            };
+            var suppliers = JsonSerializer.Deserialize<List<Supplier>>(File.ReadAllText("Data\\suppliers.json"));
+            var supplier1 = suppliers[0];
+            var supplier2 = suppliers[1];
 
             // Produkt 1 instanzieren
-            var product1 = new Product
-            {
-                Name = "Kugeschreiber gelb",
-                Price = 1.20m,
-                // Mehrere Produkte können von mehreren Lieferanten geliefert werden (N:M - Beziehung, "viele zu viele")
-                Suppliers = new List<Supplier> { supplier1, supplier2 }
-            };
+            var products = JsonSerializer.Deserialize<List<Product>>(File.ReadAllText("Data\\products.json"));
 
+            var product1 = products[0];
             // Produkt 2 instanzieren
-            var product2 = new Product
-            {
-                Name = "Druckpapier premium",
-                Price = 9.9m,
-                // Mehrere Produkte können von mehreren Lieferanten geliefert werden (N:M - Beziehung, "viele zu viele")
-                Suppliers = new List<Supplier> { supplier1 }
-            };
-
-
+            var product2 = products[1];
+           
             supplier1.Products = new List<Product> { product1, product2 };
             supplier2.Products = new List<Product> { product1 };
 
@@ -74,24 +38,11 @@ namespace MyShopApp
             // Produkte in Company einstellen
             // 1 Company kann mehrere Produkte haben (1:N - Beziehung, "eins zu viele")
 
-         
-
-
-
 
             // Kunde instanzieren
-            var customer1 = new Customer
-            {
-                Email = "aaa@bbb.at",
-                Lastname = "Mustermann",
-                Street = " Kendlerstr 34",
-                City = "Wien",
-                PostCode = 1140,
-                Phone = "000111111",
-                Firstname = "Maximilian",
-                // 1 Kunde kann nur einen KundenPass BZW: ein KundenPass kann nur einen Kunden haben (1:1 - Beziehung, "eins zu eins") 
-                CustomerCard = new CustomerCard()
-            };
+            var customers = JsonSerializer.Deserialize<List<Customer>>(File.ReadAllText("Data\\customers.json"));
+            var customer1 = customers[0];
+
 
             // Kunde der Company hinzufügen
             // 1 Company kann mehrere Kunden haben (1:N - Beziehung, "eins zu viele")
@@ -103,26 +54,25 @@ namespace MyShopApp
 
 
             // Bestellung instanzieren
-            var order1 = new Order();
+            var order1 = JsonSerializer.Deserialize<Order>(File.ReadAllText("Data\\orders.json"));
 
+            var orderItems = JsonSerializer.Deserialize<List<OrderItem>>(File.ReadAllText("Data\\orderItems.json"));
             // Bestellpostionen instanzieren
-            var orderItem1 = new OrderItem
-            {
-                Quantity = 2,
-                Product = product1
-            };
+            var orderItem1 = orderItems[0];
+            orderItem1.Product = product1;
 
-            var orderItem2 = new OrderItem
-            {
-                Quantity = 1,
-                Product = product2
-            };
+            var orderItem2 = orderItems[1];
+            orderItem2.Product = product2;
+
 
             // Bestellpostionen der Bestellung hinzufügen
             // 1 Bestellung kann viele BestellPosition haben (1:N - Beziehung, "eins zu viele") 
             order1.AddOrderItem(orderItem1);
             order1.AddOrderItem(orderItem2);
 
+            
+            
+            
             // Bestellung dem Kunden zuweisen
             // 1 Kunde kann viele Bestellgen tätigen (1:N - Beziehung, "eins zu viele") 
 
