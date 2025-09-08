8e9ee484c56e8ee4768c846c6d74120306d52eff
BucPOc <buch@aspnetprofi.com>
2025-09-08 12:44:52 +0200

    upd

--- Modul 6/Visualisierung von Daten/ChartClienseitigDemo/Models/Umsatz.cs
+++
@@ -1,9 +0,0 @@
-namespace ChartClienseitigDemo.Models
-{
-    public class Umsatz
-    {
-        public int UmsatzId { get; set; }
-        public string Monat { get; set; } = null!;
-        public int Betrag { get; set; }
-    }
-}
